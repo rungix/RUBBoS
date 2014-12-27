559cf81666fb590383924354fc33ba38b2533457
sguazt <marco.guazzone@gmail.com>
2014-12-27 14:56:06 +0100

    [servlets] (change) Update HTML and script path to something more standard

--- servlets/src/java/edu/rice/rubbos/servlets/Config.java
+++ servlets/src/java/edu/rice/rubbos/servlets/Config.java
@@ -43,8 +43,8 @@ public class Config
   {
   }
 
-  public static final String HTMLFilesPath                 = "/home/margueri/RUBBoS/Servlet_HTML";
-  public static final String DatabaseProperties            = "/home/margueri/RUBBoS/Servlets/mysql.properties";
+  public static final String HTMLFilesPath                 = "/usr/share/tomcat/webapps/rubbos";
+  public static final String DatabaseProperties            = "/usr/share/tomcat/webapps/rubbos/WEB-INF/classes/META-INF/mysql.properties";
 
   public static final int    AboutMePoolSize               = 10;
   public static final int    BrowseCategoriesPoolSize      = 6;
