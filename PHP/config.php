a6881b4207bfe5b971115f8b2a1b940974da0843
gr1x <rungix@gmail.com>
2019-03-28 09:58:39 +0800

    merge the original version to add db config.php

---
+++ PHP/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database configuration
+ *   host:     The MySQL server host
+ *   username: Username to use
+ *   password: Password of the user
+ *   db_name:  Database to use
+ */
+$DB_CONFIG = array(
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => 'YouAskMe',
+  'db_name' => 'rubbos'
+);
